9a12adc280ba225e118fa4485081e11d591c8c47
Doug Loomer <loomerds@gmail.com>
2019-05-12 17:44:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,6 +18,13 @@ function whileLoop(num) {
 }
 
 function doWhileLoop(num) {
+  var i = 0;
+ 
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+  
   do {
     console.log("I run once regardless.")
   } while(incrementVariable() < num);
